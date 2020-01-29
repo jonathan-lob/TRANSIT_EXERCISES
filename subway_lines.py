b50c0c67ea11a00d838d211fe380443bb2d2ec5f
jonathan-lob <60291619+jonathan-lob@users.noreply.github.com>
2020-01-29 17:32:46 -0500

    Update subway_lines.py

--- subway_lines.py
+++ subway_lines.py
@@ -2,23 +2,34 @@ import json
 import urllib
 import requests
 
+
+
 class rail_lines(object):
+    """ 
+        class to hold rail line types
+        chile of python default class object
+    """
+    
     def enc_req_get(self, url):
         """get API request"""
-        headers = {'Accept': 'application/json', 'Authorization': 'Bearer f9fed311243b4d7a94c9596d46b566cc'}
         key = "f9fed311243b4d7a94c9596d46b566cc"
+        headers = {'Accept': 'application/json', 'Authorization': 'Bearer {}'.format(key)}
+       
         try:
+            #make API request 
             r =  requests.get(url, headers=headers)
-            parsed_reponse=json.loads(r.content)
+            parsed_reponse = json.loads(r.content)
             return parsed_reponse, r.status_code
         except e as error:
+            #call requsts get failed-print out status code
             print(error)
             print("request status_code:" + r.status_code)
+            exit(1)
 
-    def display_get_mbta_rail_line(self, type):
+    def display_get_mbta_rail_line(self, type, attr_name):
         """
-                get _light_rail [type]=0
-                get _heavy_rail [type]=1
+            get _light_rail [type]=0
+            get _heavy_rail [type]=1
         """
         res = []
         if type == 0:
@@ -26,23 +37,25 @@ class rail_lines(object):
         else:
             rail_type = "heavy"
         rq = 'https://api-v3.mbta.com/routes?filter[type]=' + str(type)
-        #print("\n\n\n" + rail_type + " rail lines USING FILTER method for efficiency and to minimize load on API server:\n")
         data, stat = self.enc_req_get(rq)
         if stat == 200:
             ld_data = data["data"]
             for rec in ld_data:
-                res.append(rec["attributes"]["long_name"])
-                #print(rec["attributes"]["long_name"])
+                res.append(rec["attributes"][attr_name])
             return res
         else:
             print("An error occurred")
 
 class rail_line_stops(rail_lines):
-    """stops, child of rail_lines"""
+    """rail line stops, child of rail_lines"""
 
     def get_all_stops_attr(self, field_name):
+        """
+        get stop attribute
+        """
         res = []
         rq = 'https://api-v3.mbta.com/stops'
+        #return requests data and status as tuple 
         data, stat = rail_lines.enc_req_get(self, rq)
         stp_data = data["data"]
         for des in stp_data:
@@ -50,13 +63,19 @@ class rail_line_stops(rail_lines):
         return res
 
     def filter_stops_for_rail_lines(self):
+        """
+        
+        """
+        #initialize lists
         all_rail_lines = []
         all_stops_desc = []
-        rali_stop_count = {}
-        all_rail_lines = rail_lines.display_get_mbta_rail_line(self,0) + rail_lines.display_get_mbta_rail_line(self,1)
+        #initialize dictionary
+        rail_stop_count = {}
+        #get all rail lines: light and heavy
+        all_rail_lines = rail_lines.display_get_mbta_rail_line(self, 0, "long_name") + rail_lines.display_get_mbta_rail_line(self, 1, "long_name")
         all_stops_desc = self.get_all_stops_attr("description")
         for rail_line in all_rail_lines:
-            rali_stop_count[rail_line] = 0
+            rail_stop_count[rail_line] = 0
             for stop_desc in all_stops_desc:
                 try:
                     if 'Green Line' in rail_line:
@@ -65,34 +84,39 @@ class rail_line_stops(rail_lines):
                         mtch_str = '- ' + green_mtch + ' - (' 
                         if '(' + green_letter + ')' in stop_desc:
                             if mtch_str in stop_desc:
-                                rali_stop_count[rail_line] += 1
+                                rail_stop_count[rail_line] += 1
                     else:
                         mtch_str = '- ' + rail_line + ' -'
                         if mtch_str in stop_desc:
-                            rali_stop_count[rail_line] += 1
+                            rail_stop_count[rail_line] += 1
                 except:
                     tt = 0
-        return rali_stop_count
-
+        return rail_stop_count
 
 if __name__ == "__main__":
     
-    
-    print("\n\n################################Question 1 output################################\n\n")
+
+    pre_del ='\n\n################################'
+    post_del = '################################\n\n'
+    #print out Q1 resuklt
+    print("{}{}{}".format(pre_del, 'Question 1 output', post_del))
     rl = rail_lines()
     print("light rail lines USING FILTER method for efficiency and to minimize load on API server:\n\n")
-    for rail_line in rl.display_get_mbta_rail_line(0):
+    for rail_line in rl.display_get_mbta_rail_line(0, "long_name"):
         print(rail_line)
     print("\n\nheavy rail lines USING FILTER method for efficiency and to minimize load on API server:\n\n")
-    for rail_line in rl.display_get_mbta_rail_line(1):
+    for rail_line in rl.display_get_mbta_rail_line(1, "long_name"):
         print(rail_line)
     
-
-    print("\n\n\n################################Question 2 output################################\n\n")
+    #print out Q2 results
+    print("{}{}{}".format(pre_del, 'Question 2 output', post_del))
     rls = rail_line_stops()
     stops_dict=rls.filter_stops_for_rail_lines()
+    #use lambda functions to get min and max dictionary values and corresponding keys
     key_max = max(stops_dict.keys(), key=(lambda k: stops_dict[k]))
     key_min = min(stops_dict.keys(), key=(lambda k: stops_dict[k]))
-    print("max stops on a rail line:" + str(stops_dict[key_max]) + "  on the following line:" + key_max)
-    print("min stops on a rail line:" + str(stops_dict[key_min]) + "  on the following line:" + key_min )
-
+    #print out min and max
+    s1 = "stops on a rail line:"
+    s3 = "  on the following line:"
+    print("max  {}{}{}{}".format(s1, str(stops_dict[key_max]), s3, key_max))
+    print("min  {}{}{}{}".format(s1, str(stops_dict[key_min]), s3, key_min))
